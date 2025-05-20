29e5614efc7356fcc823c499a71cc61d839a02cf
matous <demkoma@seznam.cz>
2025-05-20 19:57:30 +0200

    adv: Basic question in assigning

--- main.py
+++ main.py
@@ -40,16 +40,42 @@ for worker in data["workers"]:
 
 print(possible_cleanings)
 
+assigned_cleanings = []
 for pc_key in possible_cleanings.keys():
     target_worker = data["workers"][data_prep.get_record(data["workers"], "worker", int(pc_key))[0]]
     target_worker_options = []
-    for cleaning_id in possible_cleanings[pc_key]:
+    in_work = 0
+    for idx_clid, cleaning_id in enumerate(possible_cleanings[pc_key]):
         cleaning = data["cleanings"][data_prep.get_record(data["cleanings"], "cleaning", int(cleaning_id))[0]]
         path = planner.best_path(target_worker["home"], cleaning["home"], data)
 
         if path["time"] == -1:
             # Path does not exist
             continue
+
+        if in_work + cleaning["duration"] > target_worker["hours_per_day"]:
+            # can somebody else?
+            # if so, drop
+            continue
+
+        if idx_clid == 0:
+            origin = target_worker["home"]
+        else:
+            origin = possible_cleanings[pc_key][idx_clid - 1]["home"]
+
+        destination = cleaning["home"]
+
+        # Odkud jede?
+        # Kam jede?
+        # To, kam jede, jak dlouho tam bude?
+        #   Když víc, než má pracovat, zahodit
+        # Stihne to?
+        #   Jaký má ETA?
+        #   Bude mít zpoždění?
+        #       Kolik bude to zpoždění stát? -- zatím se zpožděním vyhodit
+        # Může úklid udělat někdo jiný?
+        #   Pokud ne a nikdo nemůže udělat ani ten předchozí, nechat
+        #   Co jinak zatím nevím
 
 
 
