37810d36172d275a9be53b3d9508fc16cfe20fa1
matous <demkoma@seznam.cz>
2025-05-25 11:34:26 +0200

    adv: More penalties, even worse results

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ Předpokládám, že do pracovníkovy doby strávené prací a "od - do" se nepo
 script_start = time.time()
 result = []
 not_scheduled = []
-fname = "02"
+fname = "03"
 data = data_prep.prepare_data(f"{fname}.in")
 
 data_prep.show_data(data, "roads")
@@ -51,8 +51,6 @@ for pc_key in possible_cleanings.keys():
             print(f"Overload, in work {in_work}, target_duration: {target_worker['hours_per_day']} cleaning duration {cleaning['duration']}")
             # Going overload
             overload_penalty = cleaning["duration"] * penalties.OVERLOAD_PENALTY
-            print(overload_penalty)
-            print("/" * 5 )
 
         out_of_ideal_penalty = 0
         if cleaning["start"] > target_worker["end"] or cleaning["end"] < target_worker["start"]:
@@ -61,7 +59,7 @@ for pc_key in possible_cleanings.keys():
         elif cleaning["start"] < target_worker["start"]:
             # Start is before the window, end after
             out_of_ideal_penalty = (target_worker["start"] - cleaning["start"]) * penalties.OUT_OF_IDEAL
-        elif cleaning["start"] < target_worker["end"]:
+        elif cleaning["end"] > target_worker["end"]:
             # End is after window, start in the window
             out_of_ideal_penalty = (cleaning["end"] - target_worker["end"]) * penalties.OUT_OF_IDEAL
 
@@ -85,11 +83,19 @@ for pc_key in possible_cleanings.keys():
         time_reserve = cleaning["start"] - data["cleanings"][data_prep.get_record(data["cleanings"], "cleaning", int(possible_cleanings_unassigned[pc_key][idx_clid - 1]))[0]]["end"]
         total_points_earned = (math.floor(cleaning["duration"]) * penalties.HOUR_CLEANING + penalties.CLEANING_DONE)
         time_over = path["time"] + delay - time_reserve
+        idle_penalty = 0
+        print("time over", time_over)
+        if time_over < 0:
+            # Worker is idle
+            time_over = 0  # No late arrival
+            idle_penalty = abs(time_over) * penalties.IDLE_PENALTY
+
         travel_penalty = path["time"] * penalties.TRAVEL_PENALTY
-        netto_points = total_points_earned - (time_over * penalties.ARRIVE_LATE) - travel_penalty - overload_penalty - out_of_ideal_penalty
+        print(total_points_earned, (time_over * penalties.ARRIVE_LATE), travel_penalty, overload_penalty, out_of_ideal_penalty)
+        netto_points = total_points_earned - (time_over * penalties.ARRIVE_LATE) - travel_penalty - overload_penalty - out_of_ideal_penalty - idle_penalty
         print("-" * 20)
 
-        if netto_points < 0 < len(planner.can_do_it_someone_else(possible_cleanings, pc_key, cleaning_id)):
+        if netto_points < -penalties.CLEANING_NOT_DELIVERED and 0 < len(planner.can_do_it_someone_else(possible_cleanings, pc_key, cleaning_id)):
             print(f"Appending {cleaning_id} to not scheduled; netto points: {netto_points} < 0")
             # TODO: Count cumulative delay
             other_options = planner.can_do_it_someone_else(possible_cleanings_unassigned, pc_key, cleaning_id)
